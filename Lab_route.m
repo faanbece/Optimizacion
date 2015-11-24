d959e571f27e55b1fbb12aa90ecbc377dc325761
faanbece <faanbece@gmail.com>
2015-11-23 23:09:32 -0500

    SPEA2 - first commit

---
+++ Lab_route.m
@@ -0,0 +1,13 @@
+function route = Lab_route( pop, tam )
+%LAB_ROUTE Summary of this function goes here
+%   Detailed explanation goes here
+    r=randperm(tam)
+    for i=1:(numel(pop))
+        if (isequal(pop(i).Position,r)) 
+            r=randperm(tam);
+            i=1;
+        end
+    end
+    route=r;
+end
+
